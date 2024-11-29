79323e3e55d129206647f151988295669596abac
rjreddy111 <rjreddy111@gmail.com>
2024-11-29 15:45:08 +0530

    testing code

---
+++ server.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const path = require("path")
+
+
+
+const app = express()
+
+app.set("view engine", "ejs");
+
+app.set('views', path.join(__dirname, 'views'));
+
+
+
+const url = "http://qa-gb.api.dynamatix.com:3100/api/applications/getApplicationById/67339ae56d5231c1a2c63639"
+
+const calculateLVT = (loanRequired,purchasePrice) => (loanRequired/purchasePrice)*100
+
+const checkRulesSet = [
+
+    {
+        name : "Valuation Fee Paid", 
+        condition : (data)=> data.isValuationFeePaid===true 
+    },
+    
+     {
+        name :"UK Resident", 
+        condition:(data)=> data.isUkResident===true 
+     }, 
+
+     {
+        name:"Risk Rating Medium", 
+        condition : (data)=>data.riskRating==="Medium"
+     },
+
+     {
+        name : "LVT Below 60%", 
+        condition: (data) => calculateLVT(data.loanRequired,data.purchasePrice) < 60
+     }
+]
+
+app.get("/", async(req,res)=> {
+    
+    try {
+    const resData = await fetch (url)
+    const data = await resData.json()
+    console.log(data)
+
+    const outputResults = checkRulesSet.map((rules)=> ({
+        name : rules.name , 
+        currentStatus : rules.condition(data)? "pass" : "fail"
+    })
+)
+    res.render("index", {outputResults})
+    }
+    
+    catch(error) {
+        console.log("Error")
+        res.status(500).send("Error occured while fetching Data")
+    }
+})
+
+app.listen(5001,()=> {
+    console.log("server is running on 5001")
+
+})
+
+
+
+module.exports = app
